d5ab3c17d6f879e6a59614cba18c2d8a7d32f4c6
Gery <ardinegery@gmail.com>
2019-07-10 16:17:12 +0700

    Final hooks

---
+++ src/components/SongList.js
@@ -0,0 +1,38 @@
+import React,{useState,useEffect} from 'react';
+import uuid from 'uuid/v1';
+import NewSongForm from './NewSongForm';
+
+const SongList = () => {
+    const [songs, setSongs] = useState([
+        {title:'almost home',id:'1'},
+        {title:'memory gospel',id:'2'},
+        {title:'this wild darkness',id:'3'},
+    ])
+    const [age,setAge]=useState(20);
+    const addSong = (title) =>{
+        setSongs([...songs, {title:title, id:uuid()} ])
+    }
+    useEffect(()=>{
+        console.log("Use effect on Songs",songs);
+    },[songs])
+    useEffect(()=>{
+        console.log("Use effect on Age",age);
+    },[age])
+    return (
+        <div className="song-list">
+            <ul>
+                {
+                    songs.map(song=>{
+                        return(
+                            <li key={song.id}>{song.title}</li>
+                        )
+                    })
+                }
+            </ul>
+            <NewSongForm addSong={addSong}/>
+            <button onClick={()=>setAge(age+1)}>Add 1 age to : {age}</button>
+        </div>
+    )
+}
+
+export default SongList
